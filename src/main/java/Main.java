e02c525ef579b64d46bb4d5ed9eafb79c8a28278
KiwiUG <KiwiUG69@gmail.com>
2023-03-30 13:25:24 +0545

    username and paswword check added.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -63,13 +63,13 @@ public class Main {
 
                     break;
                     case 4:
-                        conn = DBUtils.connect();
+
                         for (User u : user_list ) {
                             System.out.println(u.getId()+"."+" Name: " + u.getUsername());
                         }
                         break;
                 case 5:
-                    conn = DBUtils.connect();
+
                     for (Classs c : class_list ) {
                         System.out.println(c.getId()+"."+" Name: " + c.getClassname());
                     }
